6d21f4ab9dacd6ce81a2619b86b6313d9162e7b6
shoaibrayeen <shoaib.mca17.du@gmail.com>
2020-01-31 07:43:55 +0530

    Removed File

--- user_admin/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-include EmailValidatable
-class User < ApplicationRecord
-    validates :name, presence: true, length: { minimum: 3}
-    validates :email, email:true, presence:true
-    validates :phone, presence: true, length: { minimum: 10, maximum: 10}
-    validates :password, presence: true, length: { minimum: 6}
-end
