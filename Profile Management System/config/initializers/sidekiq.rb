74550951bc3ef2723b982ff0c0c3e24c3d1e5d52
shoaibrayeen <shoaib.mca17.du@gmail.com>
2020-02-06 16:50:27 +0530

    Removed Bugs

--- Profile Management System/config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-require 'sidekiq'
-#require 'sidekiq-cron'
-Sidekiq.configure_client do |config|
-	config.redis = { url: "redis://localhost:6379/0"}
-end
-Sidekiq.configure_server do |config|
-	config.redis = { url: "redis://localhost:6379/0"}
-end
