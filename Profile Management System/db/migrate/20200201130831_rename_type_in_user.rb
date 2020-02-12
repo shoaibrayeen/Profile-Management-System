29f9003f307be743f92ac82e99ac87a7a86667b4
shoaibrayeen <shoaib.mca17.du@gmail.com>
2020-02-12 10:58:55 +0530

    Removed App

--- Profile Management System/db/migrate/20200201130831_rename_type_in_user.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeInUser < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :users, :type, :user_type
-  end
-end
--- Profile Management System/test/controllers/users_api_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UsersApiControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
